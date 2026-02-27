0bd8a67bbe5ab9572bd85f4b05268eda08d9d3d1
chuchulev-sky <daniel@skyeystudio.com>
2026-02-27 21:11:03 +0200

    Add UserUsecase interface for user management operations, including methods for registration, login, retrieval, update, and deletion of users.

---
+++ internal/auth/ports/usecase.go
@@ -0,0 +1,15 @@
+package ports
+
+import (
+	"context"
+
+	"github.com/chuuch/expense-tracker-backend/internal/auth/domain"
+)
+
+type UserUsecase interface {
+	Register(ctx context.Context, email, password, firstName, lastName, phone, address, city, state, zip, country string) (*domain.User, error)
+	Login(ctx context.Context, email, password string) (*domain.User, error)
+	GetByID(ctx context.Context, id string) (*domain.User, error)
+	UpdateUser(ctx context.Context, id, firstName, lastName, phone, address, city, state, zip, country string) (*domain.User, error)
+	DeleteUser(ctx context.Context, id string) error
+}
